43c398fec4e7bf98573a9b5e3ecc6a3d1a4250c8
jxrory <jxrory@jxrory.com>
2021-12-16 21:31:40 +0800

    添加@符号

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = {
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    resolve: {
+      alias: {
+        '@': resolve('src'),
+      },
+    },
+  },
+};
